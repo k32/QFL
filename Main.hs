b91b956c319d8ce7ea493f6519f06b08bfa037d3
k3 <k3628800@gmail.com>
2015-01-06 01:16:16 +0300

    added some interactivity

--- Main.hs
+++ Main.hs
@@ -1,8 +1,6 @@
-{- Wchan Fubar Compiler -}
+{- Quantum Fubar Language -}
 {-# LANGUAGE GADTs, StandaloneDeriving, UnicodeSyntax, KindSignatures,
-             FlexibleInstances, FlexibleContexts #-}
-
--- TODO: Ability to continue execution after collapse
+             FlexibleInstances, LambdaCase #-}
 
 module Main (main) where
 
@@ -11,7 +9,9 @@ import Data.List
 import Control.Monad
 import Control.Monad.Writer
 import System.Random
+import System.Posix.Signals
 import System.Environment
+import Control.Concurrent.MVar
 
 instance Show (Int → Int) where
   show _ = "<function>"
@@ -20,7 +20,7 @@ instance Eq (Int → Int) where
 
 infixl 7 :.
 
-data T ∷ * where {J, Â, Â', S, K ∷ T; (:.) ∷ T → T → T; Σ ∷ {σ ∷ String} → T
+data T ∷ * where {J, Â, Â', S, K ∷ T; (:.) ∷ T → T → T; Ψ ∷ {σ ∷ String} → T
                  ;F ∷ (Int → Int) → T; N ∷ Int → T; Ø ∷ String → T}
 deriving instance Show T
 deriving instance Eq T
@@ -34,7 +34,7 @@ parse2 ('b':'a':'r':c) t = parse2 c (Â:t)
 parse2 ('~':c) (a:b:t) = parse2 c (b:.a:t)
 parse2 ('~':_) _ = error "Parse error: missing operand(s)"
 parse2 (_:c) t = parse2 c t
-parse2 [] (h:_) = h :. Σ []
+parse2 [] (h:_) = h :. Ψ []
 parse2 [] [] = error "Parse error: empty program"
 
 s ∷ T → T
@@ -46,26 +46,51 @@ s (S :. x :. y :. z) = (x :. z) :. (y :. z)
 s (F f :. N i) = N $ f i
 s (F f :. F g) = F $ f . g
 -- IO
-s (Â' :. N i :. ψ @ (Σ {})) = ψ {σ = toEnum i : σ ψ}
+s (Â' :. N i :. ψ @ (Ψ {})) = ψ {σ = toEnum i : σ ψ}
 -- Using Â → Â' to indicate that Church → Int conversion has been started
-s (Â :. n :. ψ @ (Σ {})) = Â' :. (n :. F (+1) :. N 0) :. ψ
+s (Â :. n :. ψ @ (Ψ {})) = Â' :. (n :. F (+1) :. N 0) :. ψ
 -- Other cases
 s (a :. b) = (s a) :. (s b)
 s x = x
 
-eval ∷ T → T
-eval t | t == t' = t
-       | otherwise = eval t'
-  where t' = s t
+eval ∷ MVar Int → T → IO T
+eval i t = takeMVar i >>= \case
+  0 → return t
+  i' | t == t' → return t
+     | otherwise → putMVar i (i'-1) >> eval i t'
+    where t' = s t
 
-r' ∷ T → [String]
-r' (Σ {σ = s}) = [reverse s]
+r' ∷ T → [T]
+r' ψ@(Ψ {}) = [ψ]
+r' t@(_ :. Ψ {}) = [t]
 r' (a :. b) = r' a ++ r' b
 r' _ = []
 
-r ∷ T → IO ()
-r t = putStrLn =<< (t' !!) <$> randomRIO (0, length t' - 1)
-  where t' = r' t
+r ∷ T → IO (Maybe T)
+r t = let t' = r' t in
+  ((t' !!) <$> randomRIO (0, length t' - 1)) >>= \case
+    Ψ{σ = s}      → putStrLn (reverse s) >> return Nothing
+    t :. Ψ{σ = s} → putStrLn (reverse s) >> return (Just t)
+
+setMVar v = (tryTakeMVar v >>) . putMVar v
+
+loop ∷ MVar Int → T → IO ()
+loop c t = do
+  putStrLn $ "Waiting for decision: Abort (a), Perform measurement (m)," ++
+             " Continue (c), Run steps (number)"
+  getLine >>= \case
+   "a" → return ()
+   "m" → r t >>= \case
+     Just t'' → loop c t''
+     Nothing → return ()
+   "c" → setMVar c (-1) >> eval c t >>= loop c
+   a → case readsPrec 0 a of
+        (n,_):_ → setMVar c n >> eval c t >>= loop c
+        _ → putStrLn "Not understood." >> loop c t
 
 main ∷ IO ()
-main = r =<< eval . parse <$> (readFile =<< head <$> getArgs)
+main = do
+  cnt ← newEmptyMVar
+  installHandler keyboardSignal (Catch $ setMVar cnt (-1)) Nothing
+  t ← parse <$> (readFile =<< head <$> getArgs)
+  loop cnt t
